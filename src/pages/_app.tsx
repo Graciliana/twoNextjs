d3b09295e181447af5da76f7750eeed516196e6a
Graciliana Alves Kascher <graciliana.kascher@venturus.org.br>
2022-09-11 20:49:49 -0300

    context app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
+import { AppProvider } from '../data/context/AppContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    
+    <AppProvider>
       <Component {...pageProps} />
     
-
+</AppProvider>
   )
 }
 
--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,22 +1,20 @@
 
 import Layout from '../components/template/Layout'
 import { AppConsumer } from '../data/context/AppContext'
-
+import useAppData from '../data/hook/useAppData';
 
 export default function Notificacoes(){
- 
+ const ctx = useAppData()
   return (
-    <div >
+ 
     <Layout titulo = "Notificações"
     subtitulo="Aqui você irá gerenciar as suas notificaçoes"
     >
       <AppConsumer>
-        {dados => <h3>{dados.nome}</h3>}
+        {dados => <h3>{ctx.tema}</h3>}
         </AppConsumer>
         
-    
-
     </Layout>
-    </div>
+   
   )
 }
