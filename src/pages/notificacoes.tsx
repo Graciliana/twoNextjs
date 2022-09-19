6cf5bce6aaa78d855dc48d5ffa906d08a190680d
Graciliana <eagraciliana@gmail.com>
2022-09-18 22:37:45 -0300

    adição do prettierrc

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,16 +1,14 @@
 
 import Layout from '../components/template/Layout'
-import useAppData from '../data/hook/useAppData'
+
 
 export default function Notificacoes(){
- const { alternarTema} = useAppData()
+
   return (
     <Layout titulo = "Notificações"
     subtitulo="Aqui você irá gerenciar as suas notificaçoes"
     >
-       <button onClick={alternarTema}>
-      Alterar Tema
-       </button>
+     <h1>Notificação</h1>
     </Layout>
    
   )
